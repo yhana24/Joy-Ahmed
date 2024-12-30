807f9d0da579afb80e1c29030bf22843e2b62f69
MD JUBAED AHMED JOY <163087492+MR-JOY-420@users.noreply.github.com>
2024-12-30 20:07:44 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -13,14 +13,14 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 	const { threadID } = event;
 	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
 	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "লিভ নেউয়ার জন্য ধন্যবাদ 🤢" : "Kicked by Administrator";
+	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "কিরে😈 ${name} কোথায় পালাস  আমি জয় বট থাকতে পালাতে পারবি না বলদা🤣😂" : "Kicked by Administrator";
 	const path = join(__dirname, "joy", "leaveGif");
 	const gifPath = join(path, `l.gif`);
 	var msg, formPush
 
 	if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-	(typeof data.customLeave == "undefined") ? msg = "তুই {name} গ্রুপে থাকার যোগ্য না আবাল .\n\n{type} " : msg = data.customLeave;
+	(typeof data.customLeave == "undefined") ? msg = "কিরে😂 ${name} তোর এতো বড়ো সাহস😈 আমি জয় বট থাকতে  লিভ নেস😤থাপ্পড় মাইরা হিসু করা বন্ধ করে দিমু😒🐒.\n\n{type} " : msg = data.customLeave;
 	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
 
 	if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
