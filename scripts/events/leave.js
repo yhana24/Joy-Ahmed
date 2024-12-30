1959db2c2528fae4d2a2e1be033396c96001b066
MD JUBAED AHMED JOY <163087492+MR-JOY-420@users.noreply.github.com>
2024-12-30 20:02:21 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -2,12 +2,8 @@ module.exports.config = {
 	name: "leave",
 	eventType: ["log:unsubscribe"],
 	version: "1.0.0",
-	credits: "Mirai Team",
-	description: "Thông báo bot hoặc người rời khỏi nhóm",
-	dependencies: {
-		"fs-extra": "",
-		"path": ""
-	}
+	credits: "Joy-Ahmed",
+	description: "notify leave.",
 };
 
 module.exports.run = async function({ api, event, Users, Threads }) {
@@ -17,13 +13,14 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 	const { threadID } = event;
 	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
 	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-	const type = (event.author == event.logMessageData.leftParticipantFbId) ? " তোর এত বড় সাহস,😾 আমি জয় বট থাকতে লিভ নিবি 😹" : "বাল পাকনামির কারণে কিক খাইলো🤧";
-	const gifPath = join(path, `/noprefix/leave.mp4`);
+	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "লিভ নেউয়ার জন্য ধন্যবাদ 🤢" : "Kicked by Administrator";
+	const path = join(__dirname, "joy", "leaveGif");
+	const gifPath = join(path, `l.gif`);
 	var msg, formPush
 
 	if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-	(typeof data.customLeave == "undefined") ? msg = "ইস {name} {type} 🤖." : msg = data.customLeave;
+	(typeof data.customLeave == "undefined") ? msg = "তুই {name} গ্রুপে থাকার যোগ্য না আবাল .\n\n{type} " : msg = data.customLeave;
 	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
 
 	if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
